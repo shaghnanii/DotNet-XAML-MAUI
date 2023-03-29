ef170108ab5d3bd5a237db03bc7bd1f4df370a99
shaghnanii <shakeel.signup@gmail.com>
2023-03-29 11:18:02 +0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IntegrateX;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IntegrateX;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
