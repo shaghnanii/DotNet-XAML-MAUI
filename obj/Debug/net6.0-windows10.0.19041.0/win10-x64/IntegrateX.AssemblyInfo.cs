74babe957caede1fa1e9c8bc84d4bf0fa267a6ac
shaghnanii <shakeel.signup@gmail.com>
2023-03-30 12:33:00 +0500

    updated skeleton

--- obj/Debug/net6.0-windows10.0.19041.0/win10-x64/IntegrateX.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows10.0.19041.0/win10-x64/IntegrateX.AssemblyInfo.cs
@@ -11,10 +11,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "072d687c-ad3b-4a19-b2c0-d13dce42a520")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "IntegrateX")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegrateX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegrateX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegrateX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
